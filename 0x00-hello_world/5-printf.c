587922365e4ca30062061f12efe431487b26da80
andrexox <ogundeleifeoluwa5@gmail.com>
2021-08-26 03:37:47 -0700

    Script 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * prints out result using puts
+ *
+ * Return: 0 (successful)
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art, \n");
+return (0);
+}
