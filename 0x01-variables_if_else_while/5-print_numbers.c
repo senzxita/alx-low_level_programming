b4093497754a891129976d752e77a03d7ac2e0ef
andrexox <ogundeleifeoluwa5@gmail.com>
2021-08-27 07:34:05 -0700

    Script 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers
+ *
+ * Return: 0 (Successful)
+ */
+
+int main(void)
+{
+int i;
+
+i = 48;
+
+while (i < 58)
+{
+putchar(i);
+i++;
+}
+
+putchar(10);
+
+return (0);
+}
