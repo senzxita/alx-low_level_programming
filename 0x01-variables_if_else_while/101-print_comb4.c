867df555e7c3a85960fc5e88fa22663ba191a84c
andrexox <ogundeleifeoluwa5@gmail.com>
2021-08-27 08:16:43 -0700

    Script: Advanced 2

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of three digits
+ *
+ * Return: 0 (Successful)
+ */
+
+int main(void)
+{
+int i, j, k;
+
+i = 48;
+j = 48;
+k = 48;
+
+while (i < 58)
+{
+j = i + 1;
+while (j < 58)
+{
+k = j + 1;
+while (k < 58)
+{
+putchar(i);
+putchar(j);
+putchar(k);
+if (i < 55 || j < 56 || k < 57)
+{
+putchar(44);
+putchar(32);
+}
+k++;
+}
+
+j++;
+}
+
+i++;
+}
+
+putchar(10);
+
+return (0);
+}
