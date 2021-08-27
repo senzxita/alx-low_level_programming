ae5e38c3166d219dd3bcc81571797b68d6681a09
andrexox <ogundeleifeoluwa5@gmail.com>
2021-08-27 07:36:51 -0700

    Script 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digit numbers of base 10 from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+
+i = 48;
+
+while (i < 58)
+{
+putchar(i);
+i++;
+}
+
+putchar(10);
+
+return (0);
+}
