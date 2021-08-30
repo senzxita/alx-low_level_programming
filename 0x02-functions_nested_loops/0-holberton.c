390a48370f956fea90ec8a582ff01eba3363eb2c
andrexox <ogundeleifeoluwa5@gmail.com>
2021-08-30 14:54:06 -0700

    Script 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-	char text[10] = "Holberton";
-	int i = 0;
+char text[10] = "Holberton";
+int i = 0;
 
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(text[i]);
-	}
-	_putchar('\n');
+for (i = 0; i < 9; i++)
+{
+	_putchar(text[i]);
+}
+_putchar('\n');
 
-	return (0);
+return (0);
 }
