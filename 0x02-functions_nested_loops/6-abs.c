c0563002dc931982ad14d3123aaee011fe9ea5a7
andrexox <ogundeleifeoluwa5@gmail.com>
2021-08-31 11:35:13 -0700

    Script 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,24 +4,21 @@
  * print_sign - prints the sign of a number
  * @n: n is an integer
  *
- * Return: 1 if positive, 0 if zero, -1 if negative
+ * Return: integer
  */
 
-int print_sign(int n)
+int _abs(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
-		return (1);
+		return (n);
 	}
 	else if (n < 0)
 	{
-		_putchar('-');
-		return (-1);
+		return (-n);
 	}
 	else
 	{
-		_putchar('0');
-		return (0);
+		return (n);
 	}
 }
