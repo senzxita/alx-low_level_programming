a8a19dc64c8319fd3198acec88fe91bbeefe7489
andrexox <ogundeleifeoluwa5@gmail.com>
2021-09-06 02:25:19 -0700

    Script 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line.
+ * @str: input string.
+ *
+ * Return: no return.
+ */
+void _puts(char *str)
+{
+while (*str)
+
+_putchar(*str++);
+_putchar('\n');
+}
