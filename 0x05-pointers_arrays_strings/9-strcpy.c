cbb501adabf5fc2fc9f86b6ae24dadbbdf9e4aa0
andrexox <ogundeleifeoluwa5@gmail.com>
2021-09-06 03:44:07 -0700

    Script 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_strcpy - copies the string pointed to by src
+ *@dest: pointer char void
+ *@src: pointer char source
+ *
+ *Return: dest data
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, j = 0;
+
+	while (src[j])
+		j++;
+
+	for (i = 0; i <= j; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
