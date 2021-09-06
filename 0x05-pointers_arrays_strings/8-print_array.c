b5872837c83fe39af6f92ff588151318ec0fab24
andrexox <ogundeleifeoluwa5@gmail.com>
2021-09-06 03:37:36 -0700

    Script 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *print_array - prints n elements of an array of integers
+ *@a: pointer integer
+ *@n: parameter integer
+ *
+ *Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+
+	putchar('\n');
+}
